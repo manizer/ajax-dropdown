3082f55ff0fc75f62832aa8327052ad6d93bbbff
Anthony Gilrandy Theo <anthony.theo@binus.edu>
2019-10-31 15:30:44 +0700

    Add project files.

---
+++ ajax-dropdown/Controllers/DropdownController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ajax_dropdown.Controllers
+{
+    public class DropdownController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
